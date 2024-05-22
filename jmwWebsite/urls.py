78336db1ea3bd2b21ae962134441fdbbefd4774f
Shae Kuhnke <sjkuhnke@uwm.edu>
2024-05-22 11:55:11 -0500

    Configured urls.py and views.py, added templates for 7 pages, 3/7 implemented

--- jmwWebsite/urls.py
+++ jmwWebsite/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from jmwapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('rent-lease/', views.rent_lease, name='rent_lease'),
+    path('jobs/', views.jobs, name='jobs'),
 ]
--- jmwapp/views.py
+++ jmwapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def rent_lease(request):
+    return render(request, 'rent_lease.html')
+
+
+def jobs(request):
+    return render(request, 'jobs.html')
